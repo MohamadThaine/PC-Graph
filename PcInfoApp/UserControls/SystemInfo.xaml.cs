517fed58e02b03ef22325f611aef27d794f61bcd
hikikun11 <hamwd21@gmail.com>
2022-10-18 02:12:46 +0300

    some design chnages

--- PcInfoApp/UserControls/SystemInfo.xaml.cs
+++ PcInfoApp/UserControls/SystemInfo.xaml.cs
@@ -13,47 +13,5 @@ namespace PCGraph.UserControls
         {
             InitializeComponent();
         }
-
-        private void ExpanderExpanded(object sender, RoutedEventArgs e)
-        {
-            double height = 210;
-            Expander GetNameEx = (Expander)sender;
-            string Name = GetNameEx.Name;
-            if (Name == "CPUExpander")
-                CpuBorderVisual.Height = height;
-            else if (Name == "GPUExpander")
-                GpuBorderVisual.Height = height;
-            else if (Name == "MBExpander")
-                MBBorderVisual.Height = height;
-            else if (Name == "RAMExpander")
-                RamBorderVisual.Height = height + 50;
-            else if (Name == "STORAGEExpander")
-                STORAGEBorderVusual.Height = height;
-            else if (Name == "AUDIOExpander")
-                AudioBorderVisual.Height = height;
-            else if (Name == "NETWORKExpander")
-                NETWORKBorderVisual.Height = height;
-        }
-
-        private void ExpanderCollepsed(object sender, RoutedEventArgs e)
-        {
-            double height = 110;
-            Expander GetNameEx = (Expander)sender;
-            string Name = GetNameEx.Name;
-            if (Name == "CPUExpander")
-                CpuBorderVisual.Height = height;
-            else if (Name == "GPUExpander")
-                GpuBorderVisual.Height = height;
-            else if (Name == "MBExpander")
-                MBBorderVisual.Height = height;
-            else if (Name == "RAMExpander")
-                RamBorderVisual.Height = height;
-            else if (Name == "STORAGEExpander")
-                STORAGEBorderVusual.Height = height;
-            else if (Name == "AUDIOExpander")
-                AudioBorderVisual.Height = height;
-            else if (Name == "NETWORKExpander")
-                NETWORKBorderVisual.Height = height;
-        }
     }
 }
